79adab261bb91c4badce25cef7da05129177e448
Ariel <ariel040132@gmail.com>
2023-05-15 15:25:03 +0800

    feat: add edit router

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -12,6 +12,12 @@ router.get("/new", (req, res) => {
 });
 router.post("/new", (req, res) => {
   //const userId = req.user._id;
+  const { name, date, categoryId, amount } = req.body;
+  if (!name || !date || !categoryId || !amount) {
+    return Category.findById(categoryId)
+      .lean()
+      .then(() => res.render(res.render("new", { name, date, amount })));
+  }
   Expense.create(req.body)
     .then(() => res.redirect("/"))
     .catch((err) => {
@@ -19,6 +25,27 @@ router.post("/new", (req, res) => {
       res.redirect("/"); // 或者顯示錯誤訊息給使用者
     });
 });
-
+//! 編輯功能
+router.get("/:id/edit", (req, res) => {
+  const _id = req.params.id;
+  Category.find()
+    .lean()
+    .then((category) => {
+      return Expense.findOne({ _id })
+        .lean()
+        .then((expenses) => res.render("edit", { expenses, category }));
+    });
+});
+router.put("/:id", (req, res) => {
+  const _id = req.params.id;
+  //const userId = req.user._id;
+  //const reqUpdateObj = { userId, ...req.body };
+  Expense.findByIdAndUpdate(_id, req.body)
+    .then((data) => {
+      console.log("data is =", data);
+      res.redirect("/"); //`/expense/${data._id}`
+    })
+    .catch((err) => console.log(err));
+});
 //*底部
 module.exports = router;
